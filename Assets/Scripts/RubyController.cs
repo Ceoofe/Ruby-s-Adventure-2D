27739aa0c2c7e80d8a30fa72c0897ca4a46e1756
Ceoofe <146845430+Ceoofe@users.noreply.github.com>
2023-10-20 09:11:57 -0400

    Box prefabs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,11 +13,15 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks for left/right keys for the player
         float horizontal = Input.GetAxis("Horizontal");
+        // Check for up/down keys OR W/S keys
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
+        // Moves the player on the x-axis
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        // Moves the player on the y-axis
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
